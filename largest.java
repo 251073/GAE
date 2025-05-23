64119bf607efc02a79be0bbc0a2d5db253e76a7e
251073 <cse251073@saranathan.ac.in>
2025-05-23 11:03:02 +0530

    largest.java

--- largest.java
+++ largest.java
@@ -12,10 +12,11 @@ for(int i=0;i<n;i++){
 if(a[i]>max1){
 max1=a[i];
 }
-else(a[i]<max1 && a[i]>max2){
+else if(a[i]<max1 && a[i]>max2){
 max2=a[i];
 }
 }
+System.out.println();
 System.out.println(max2);
 }
 }
